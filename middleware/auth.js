0cdac8d7e878b7e78c436e50b65b238e2653aeb8
unknown <muhammedadam4544@gmail.com>
2023-07-14 18:33:25 +0100

    auth middleware fix two

--- middleware/auth.js
+++ middleware/auth.js
@@ -2,28 +2,26 @@ import authStore from "~/store/auth-store";
 
 export default defineNuxtRouteMiddleware(async (to, _) => {
 	const nuxtApp = useNuxtApp();
-	nuxtApp.hook("page:start", async () => {
-		await useAsyncData("authMiddleware", async () => {
-			const config = useRuntimeConfig();
-			const headers = useRequestHeaders(["cookie"]);
+	await useAsyncData("authMiddleware", async () => {
+		const config = useRuntimeConfig();
+		const headers = useRequestHeaders(["cookie"]);
 
-			const auth = authStore();
+		const auth = authStore();
 
-			const url = process.server
-				? process.env.ENDPOINT_URL
-				: config.public.ENDPOINT_URL;
+		const url = process.server
+			? process.env.ENDPOINT_URL
+			: config.public.ENDPOINT_URL;
 
-			try {
-				await auth.getCsrfToken(url, headers);
-				console.log(auth.isLoggedIn);
-				auth.testMessage = "working";
-			} catch (error) {
-				await auth.logUserOut(url, headers);
-				if (to.meta.requiresAuth) {
-					await navigateTo("/?auth=false");
-				}
+		try {
+			await auth.getCsrfToken(url, headers);
+			console.log(auth.isLoggedIn);
+			auth.testMessage = "working";
+		} catch (error) {
+			await auth.logUserOut(url, headers);
+			if (to.meta.requiresAuth) {
+				await navigateTo("/?auth=false");
 			}
-		}); 
+		}
 	});
 	await nuxtApp.callHook("page:start");
 });
