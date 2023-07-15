f22963cdcde1d30ac8523711543b493fe2cf67b4
unknown <muhammedadam4544@gmail.com>
2023-07-15 14:11:50 +0100

    auth middleware fix two

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,24 +1,26 @@
 import authStore from "~/store/auth-store";
 
 export default defineNuxtRouteMiddleware(async (to, _) => {
-	const nuxtApp = useNuxtApp();
+	if (process.client) {
+		const nuxtApp = useNuxtApp();
 
-	const config = useRuntimeConfig();
-	const headers = useRequestHeaders(["cookie"]);
+		const config = useRuntimeConfig();
+		const headers = useRequestHeaders(["cookie"]);
 
-	const auth = authStore();
+		const auth = authStore();
 
-	const url = process.server
-		? process.env.ENDPOINT_URL
-		: config.public.ENDPOINT_URL;
+		const url = process.server
+			? process.env.ENDPOINT_URL
+			: config.public.ENDPOINT_URL;
 
-	try {
-		await auth.getCsrfToken(url, headers);
-		console.log(auth.isLoggedIn);
-	} catch (error) {
-		await auth.logUserOut(url, headers);
-		if (to.meta.requiresAuth) {
-			await navigateTo("/?auth=false");
+		try {
+			await auth.getCsrfToken(url, headers);
+			console.log(auth.isLoggedIn);
+		} catch (error) {
+			await auth.logUserOut(url, headers);
+			if (to.meta.requiresAuth) {
+				await navigateTo("/?auth=false");
+			}
 		}
 	}
 });
